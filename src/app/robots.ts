5a5081197d516b2e5509bfba171ff8e666754312
BoweloIPSPACE <medicalappmedapp@gmail.com>
2026-08-24 21:25:10 +0300

    Use current production URL in robots metadata

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,6 +1,5 @@
+import { INTELLECTX_PUBLIC_SITE_URL } from "@/lib/site-config";
 import type { MetadataRoute } from "next";
-
-const siteUrl = "https://intellect-x-coral.vercel.app";
 
 export default function robots(): MetadataRoute.Robots {
   return {
@@ -8,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: `${siteUrl}/sitemap.xml`,
+    sitemap: `${INTELLECTX_PUBLIC_SITE_URL}/sitemap.xml`,
   };
 }
